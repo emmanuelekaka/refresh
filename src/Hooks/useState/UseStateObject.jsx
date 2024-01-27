1e43fec8300068ab7577ba504739ec10c3e43593
EmmaFrost <ekakashean@gmail.com>
2024-01-27 11:09:05 +0300

    Chore: useStateObjects covered

--- src/Hooks/useState/UseStateObject.jsx
+++ src/Hooks/useState/UseStateObject.jsx
@@ -3,16 +3,27 @@ import React, {useState} from 'react'
 const UseStateObject = () => {
 
     const [person, setPerson] = useState({name:"John Doe",age:0, address:'kla'})
-    let updateName = ()=>{
-        setPerson({...person, name:"Emma Emma"});
-    }
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+    const [address, setAddress] = useState('')
 
+    let updateName = ()=>{
+      setPerson({...person, name:"Emma Emma"});
+    }
+    let updateAge = ()=>{
+      setPerson({...person, age:34});
+    }
+    let updateAddress = ()=>{
+      setPerson({...person, address:'NY'});
+    }
   return (
     <>
         <p>{person.name}</p>
         <p>{person.age}</p>
         <p>{person.address}</p>
-        <button onClick={updateName}>Update</button>
+        <button onClick={updateName}>Update Name</button>
+        <button onClick={updateAge}>Update Age</button>
+        <button onClick={updateAddress  }>Update Address</button>
     </>
   )
 }
